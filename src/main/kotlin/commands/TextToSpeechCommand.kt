f138991bc40937c1c3792c588840e6da6895aa2e
marc <@>
2022-06-15 16:36:07 +0200

    Changed mp3 metadata extraction library

--- src/main/kotlin/commands/TextToSpeechCommand.kt
+++ src/main/kotlin/commands/TextToSpeechCommand.kt
@@ -2,19 +2,24 @@ package commands
 
 import BotConfig
 import Command
+import com.adamratzman.spotify.utils.Language
 import httpClient
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.utils.io.jvm.javaio.*
 import javazoom.jl.player.Player
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import logger
+import org.jaudiotagger.audio.AudioFileIO
+import org.jaudiotagger.audio.mp3.MP3AudioHeader
+import java.io.File
 import javax.sound.sampled.AudioSystem
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
 
 @Serializable
 private data class TtsRequest(
@@ -27,6 +32,13 @@ private data class TtsResponse(
     val success: Boolean,
     @SerialName("speak_url") val speakUrl: String
 )
+
+private data class TtsQueueEntry(
+    val file: File,
+    val duration: Duration
+)
+
+private val ttsFileQueue = mutableListOf<TtsQueueEntry>()
 
 val textToSpeechCommand = Command(
     names = listOf("tts", "texttospeech"),
@@ -54,12 +66,14 @@ val textToSpeechCommand = Command(
             logger.info("Streamlabs returned URL '$url'.")
 
             withContext(Dispatchers.IO) {
-                val ttsDataInputStream = httpClient.get<HttpResponse>(url).content.toInputStream().buffered()
-                val ttsSpeechDuration = (AudioSystem.getAudioFileFormat(ttsDataInputStream).properties()["duration"] as Long).seconds
+                val ttsDataFile = File.createTempFile("tts_", ".mp3").apply {
+                    writeBytes(httpClient.get<HttpResponse>(url).content.toInputStream().readAllBytes())
+                }
 
+                val ttsSpeechDuration = AudioFileIO.read(ttsDataFile).audioHeader.trackLength.seconds
                 addedUserCooldown = ttsSpeechDuration * 5
 
-                Player(ttsDataInputStream).play()
+                ttsFileQueue.add(TtsQueueEntry(ttsDataFile, ttsSpeechDuration))
             }
         } catch (e: Exception) {
             chat.sendMessage(BotConfig.channel, "Unable to play TTS.")
@@ -67,3 +81,20 @@ val textToSpeechCommand = Command(
         }
     }
 )
+
+val ttsPlayerCoroutineScope = CoroutineScope(Dispatchers.IO)
+
+@Suppress("unused")
+val ttsPlayerJob = ttsPlayerCoroutineScope.launch {
+    while (isActive) {
+        ttsFileQueue.removeFirstOrNull()?.let { entry ->
+            entry.file.inputStream().use {
+                Player(it).play()
+            }
+
+            delay(entry.duration + 3.seconds)
+        } ?: run {
+            delay(1.seconds)
+        }
+    }
+}
